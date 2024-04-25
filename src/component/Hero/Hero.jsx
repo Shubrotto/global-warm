9b3ba0cec51da91511aa252add295fb9f81bf5ec
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2024-04-25 15:27:02 +0600

    hero design

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -1,7 +1,51 @@
 import "./hero.scss";
+import { climateChanges } from "../../data";
+import Carousel from "react-material-ui-carousel";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaLinkedinIn,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 const Hero = () => {
-  return <div className="hero_container">Hero</div>;
+  return (
+    <div className="hero_container">
+      <div className="social_icons">
+        <span>
+          Facebook
+          <FaFacebookSquare className="icons" />
+        </span>
+        <span>
+          Twitter
+          <FaTwitterSquare className="icons" />
+        </span>
+        <span>
+          Github
+          <FaGithubSquare className="icons" />
+        </span>
+        <span>
+          LinkedIn
+          <FaLinkedinIn className="icons" />
+        </span>
+      </div>
+      <Carousel className="hero_wrapper">
+        {climateChanges.map((item) => (
+          <>
+            <div className="hero_item" key={item.id}>
+              <div className="hero_item_left">
+                <h2>{item.title}</h2>
+                <p>{item.desc}</p>
+              </div>
+              <div className="hero_item_right">
+                <img src={item.img} alt="" />
+              </div>
+            </div>
+          </>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default Hero;
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const climateChanges = [
+  {
+    id: 1,
+    title: "Hotter temperatures...",
+    desc: "Wildfires start more easily and spread more rapidly when conditions are hotter. Temperatures in the Arctic have warmed at least twice as fast as the global average.",
+    img: "https://imgs.mongabay.com/wp-content/uploads/sites/20/2023/04/28125139/Bangladesh-heat-wave-3.jpg",
+  },
+  {
+    id: 2,
+    title: "More severe storms...",
+    desc: "Cyclones, hurricanes, and typhoons feed on warm waters at the ocean surface. Such storms often destroy homes and communities, causing deaths and huge economic losses.",
+    img: "https://storage.googleapis.com/afs-prod/media/5c0e6e56afd44c7c8838d96a085f3219/3000.jpeg",
+  },
+  {
+    id: 3,
+    title: "Increased drought...",
+    desc: "Deserts are expanding, reducing land for growing food. Many people now face the threat of not having enough water on a regular basis.",
+    img: "https://res.cloudinary.com/uktv/image/upload/h_720,q_80,w_1280/v1375262244/bwticffdpi5jxnunurzx.jpg",
+  },
+  {
+    id: 4,
+    title: "A warming, rising ocean",
+    desc: ". In addition, the ocean absorbs carbon dioxide, keeping it from the atmosphere. But more carbon dioxide makes the ocean more acidic, which endangers marine life and coral reefs.",
+    img: "https://www.whoi.edu/wp-content/uploads/2021/08/GreenlandCover_C16x9.jpg",
+  },
+  {
+    id: 5,
+    title: "Loss of species...",
+    desc: "Forest fires, extreme weather, and invasive pests and diseases are among many threats related to climate change. Some species will be able to relocate and survive, but others will not.",
+    img: "https://cdn.mos.cms.futurecdn.net/MHrHHVELvZuFzvPiYKT6SB-1200-80.jpg",
+  },
+  {
+    id: 6,
+    title: "Not enough food...",
+    desc: "Heat stress can diminish water and grasslands for grazing, causing declining crop yields and affecting livestock.",
+    img: "https://admin.concern.net/sites/default/files/styles/1120x630/public/media/images/2022-05/concern-kenya-turkana-drought.jpeg?changed=2022-05-25T14:42:38+00:00",
+  },
+  {
+    id: 7,
+    title: "More health risks...",
+    desc: "Changing weather patterns are expanding diseases, and extreme weather events increase deaths and make it difficult for health care systems to keep up.",
+    img: "https://remezcla.com/wp-content/uploads/2023/06/Mexico-Heat-Wave-Culture.jpg",
+  },
+  {
+    id: 8,
+    title: "Poverty and displacement...",
+    desc: "Most refugees come from countries that are most vulnerable and least ready to adapt to the impacts of climate change.",
+    img: "https://www.usip.org/sites/default/files/styles/image_with_caption/public/2021-09/20210913-nyt-bangladesh_9-ac.jpg?itok=b9x3xtLo",
+  },
+];
